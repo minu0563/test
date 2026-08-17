3f3679030e043bdb14fd81e3f46e0e8367446129
minu0563 <dgwag4977@gmail.com>
2026-08-18 04:55:44 +0900

    commit

--- lib/auth.ts
+++ lib/auth.ts
@@ -18,4 +18,15 @@ export const authOptions: AuthOptions = {
   session: {
     strategy: "jwt",
   },
+
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) token.sub = user.id;
+      return token;
+    },
+    async session({ session, token }) {
+      if (session.user && token.sub) session.user.id = token.sub;
+      return session;
+    },
+  },
 };
